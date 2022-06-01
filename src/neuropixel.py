758ed9b4f607210fa00071e3ef035dc66a504836
olivier <olivier.winter@hotmail.fr>
2022-06-01 10:24:17 +0100

    NP2.4 probes can have single shank geometry

--- src/neuropixel.py
+++ src/neuropixel.py
@@ -439,7 +439,7 @@ def rc2xy(row, col, version=1):
     return {'x': x, 'y': y}
 
 
-def dense_layout(version=1):
+def dense_layout(version=1, nshank=1):
     """
     Returns a dense layout indices map for neuropixel, as used at IBL
     :param version: major version number: 1 or 2 or 2.4
@@ -449,10 +449,11 @@ def dense_layout(version=1):
           'row': np.floor(np.arange(NC) / 2),
           'shank': np.zeros(NC)}
 
-    if version == 2:
+    if version == 1:  # version 1 has a dense layout, checkerboard pattern
+        ch.update({'col': np.tile(np.array([2, 0, 3, 1]), int(NC / 4))})
+    elif np.floor(version) == 2 and nshank == 1:  # single shank NP1 has 2 columns in a dense patter
         ch.update({'col': np.tile(np.array([0, 1]), int(NC / 2))})
-    elif version == 2.4:
-        # the 4 shank version default is rather complicated
+    elif np.floor(version) == 2 and nshank == 4:  # the 4 shank version default is rather complicated
         shank_row = np.tile(np.arange(NC / 16), (2, 1)).T[:, np.newaxis].flatten()
         shank_row = np.tile(shank_row, 8)
         shank_row += np.tile(np.array([0, 0, 1, 1, 0, 0, 1, 1])[:, np.newaxis], (1, int(NC / 8))).flatten() * 24
@@ -460,8 +461,6 @@ def dense_layout(version=1):
             'col': np.tile(np.array([0, 1]), int(NC / 2)),
             'shank': np.tile(np.array([0, 1, 0, 1, 2, 3, 2, 3])[:, np.newaxis], (1, int(NC / 8))).flatten(),
             'row': shank_row})
-    elif version == 1:
-        ch.update({'col': np.tile(np.array([2, 0, 3, 1]), int(NC / 4))})
     # for all, get coordinates
     ch.update(rc2xy(ch['row'], ch['col'], version=version))
     return ch
@@ -492,13 +491,14 @@ def adc_shifts(version=1):
     return sample_shift, adc
 
 
-def trace_header(version=1):
+def trace_header(version=1, nshank=1):
     """
     Returns the channel map for the dense layout used at IBL
     :param version: major version number: 1 or 2
+    :param nshank: (defaults 1) number of shanks for NP2
     :return: , returns a dictionary with keys
     x, y, row, col, ind, adc and sampleshift vectors corresponding to each site
     """
-    h = dense_layout(version=version)
+    h = dense_layout(version=version, nshank=nshank)
     h['sample_shift'], h['adc'] = adc_shifts(version=version)
     return h
