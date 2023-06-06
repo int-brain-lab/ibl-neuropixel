6c0f5f6a796d32a3b8e8a00de02b5f856ba64783
chris-langfield <christopher.langfield@internationalbrainlab.org>
2023-06-06 17:43:38 -0400

    initial add test and comment function

--- src/neurodsp/spiketrains.py
+++ src/neurodsp/spiketrains.py
@@ -25,12 +25,14 @@ def spiketrain_intersect(samples1, channels1, samples2, channels2, samples_binsi
     assert samples2.shape == channels2.shape, "samples2 and channels2 must have the same shape."
 
     if not samples_binsize:
-        # SpikeInterface default: 0.4 ms
+        # Default: 0.4 ms
         samples_binsize = int(0.4 * fs / 1000)
 
     max_samples = max(np.max(samples1), np.max(samples2))
     bins2d_shape = (int(max_samples // samples_binsize) + 2, int(num_channels // channels_binsize) + 2)
 
+    # array of bin shifts in time and channel directions
+    # applied to catch spikes on either side of bin borders
     shifts = np.array(
         [
             [0,0],
@@ -40,11 +42,20 @@ def spiketrain_intersect(samples1, channels1, samples2, channels2, samples_binsi
         ]
     )
 
+    # we'll divide by this factor and take the floor to find the sample and channel bin
+    # for each spike
     factor = np.array([[samples_binsize, channels_binsize]*4], dtype=np.float64)
 
+    # tile data over the 4 possible shift conditions
+    # What we end up with is an array that looks like:
+    # [sample_bins_shift(0,0)]
+    # [channel_bins_shift(0,0)]
+    # [sample_bins_shift(1,0)]
+    # ...
     data1 = np.tile(np.vstack((samples1, channels1)), (4,1))
     bins1 = np.floor((data1 + shifts.flatten()[:, None]) / factor.T).astype(int)
 
+    # store the ravelled (sample bin, channel bin) indices for each shift
     linear_indices1 = []
     for i in range(4):
         idx = slice(i*2, (i+1)*2)
@@ -58,6 +69,7 @@ def spiketrain_intersect(samples1, channels1, samples2, channels2, samples_binsi
         idx = slice(i*2, (i+1)*2)
         linear_indices2.append(np.ravel_multi_index(bins2[idx], bins2d_shape))
 
+    # find the intersection of the two spike trains' bin indices for each shift
     bin_linear_indices = []
     indices1 = []
     indices2 = []
@@ -67,6 +79,7 @@ def spiketrain_intersect(samples1, channels1, samples2, channels2, samples_binsi
         indices1.append(idx1)
         indices2.append(idx2)
 
+    # take the union over shifts 
     common_spike_bins = list(zip(*np.unravel_index(reduce(np.union1d, bin_linear_indices), bins2d_shape)))
     indices1_foundby2 = reduce(np.union1d, indices1)
     indices2_foundby1 = reduce(np.union1d, indices2)
--- src/tests/unit/cpu/test_dsp.py
+++ src/tests/unit/cpu/test_dsp.py
@@ -8,6 +8,7 @@ import neurodsp.utils as utils
 import neurodsp.voltage as voltage
 import neurodsp.cadzow as cadzow
 import neurodsp.smooth as smooth
+import neurodsp.spiketrains as spiketrains
 
 
 class TestSyncTimestamps(unittest.TestCase):
@@ -393,6 +394,21 @@ class TestStack(unittest.TestCase):
 class TestSpikeTrainIntersect(unittest.TestCase):
 
     def test_spiketrain_intersect(self):
-        samples_binsize = 12
-        channels_binsize = 4
-        common_spikes = []
+        spike_train1 = np.array([11, 30, 63, 80, 90, 99])
+        spike_train2 = np.array([12, 40, 68, 93, 98])
+        channels1 = np.array([30, 125, 300, 150, 340, 127])
+        channels2 = np.array([30, 123, 302, 150, 335])
+        # (11,30) - (13, 30) are separated by a time bin border, make sure we catch
+        # (40,125) - (40, 123) are separated by a channel bin border, make sure we catch
+        # (63,68) - (300,302) are within a single bin, make sure we catch
+        # (80,93) - (150,150) are too far away in time, make sure we *don't* catch
+        # (90,340) - (98,335) are too far away in channels, make sure we *don't* catch
+        # (99, 127) is an extra spike in spike train 1 that doesn't match any
+
+        common_spikes = [(0,7), (3, 30), (5, 75)]
+        # in both spike trains, the first 3 spikes are found by the other sorter
+        indices = np.array([0, 1, 2])
+        result_spikes, result_indices1, result_indices2 = spiketrains.spiketrain_intersect(
+                spike_train1, channels1, spike_train2, channels2
+            )
+        assert common_spikes == result_spikes
