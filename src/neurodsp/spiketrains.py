cfe997bd9c921d3397ab608ade0b3adff6ea4012
chris-langfield <christopher.langfield@internationalbrainlab.org>
2023-06-13 13:41:39 -0400

    add new spike binning idea

--- src/neurodsp/spiketrains.py
+++ src/neurodsp/spiketrains.py
@@ -1,5 +1,6 @@
 import numpy as np
 from functools import reduce
+from scipy.stats import mode
 
 
 def spiketrain_intersect(
@@ -47,43 +48,24 @@ def spiketrain_intersect(
         int(num_channels // channels_binsize) + 2,
     )
 
-    # array of bin shifts in time and channel directions
-    # applied to catch spikes on either side of bin borders
-    shifts = np.array(
-        [
-            [0, 0],
-            [samples_binsize // 2, channels_binsize // 2],
-            [samples_binsize // 2, 0],
-            [0, channels_binsize // 2],
-        ]
+    linear_indices1 = bin_spikes(
+        samples1,
+        channels1,
+        samples_binsize=samples_binsize,
+        channels_binsize=channels_binsize,
+        fs=fs,
+        num_channels=num_channels,
+        max_samples=max_samples,
     )
-
-    # we'll divide by this factor and take the floor to find the sample and channel bin
-    # for each spike
-    factor = np.array([[samples_binsize, channels_binsize] * 4], dtype=np.float64)
-
-    # tile data over the 4 possible shift conditions
-    # What we end up with is an array that looks like:
-    # [sample_bins_shift(0,0)]
-    # [channel_bins_shift(0,0)]
-    # [sample_bins_shift(1,0)]
-    # ...
-    data1 = np.tile(np.vstack((samples1, channels1)), (4, 1))
-    bins1 = np.floor((data1 + shifts.flatten()[:, None]) / factor.T).astype(int)
-
-    # store the ravelled (sample bin, channel bin) indices for each shift
-    linear_indices1 = []
-    for i in range(4):
-        idx = slice(i * 2, (i + 1) * 2)
-        linear_indices1.append(np.ravel_multi_index(bins1[idx], bins2d_shape))
-
-    data2 = np.tile(np.vstack((samples2, channels2)), (4, 1))
-    bins2 = np.floor((data2 + shifts.flatten()[:, None]) / factor.T).astype(int)
-
-    linear_indices2 = []
-    for i in range(4):
-        idx = slice(i * 2, (i + 1) * 2)
-        linear_indices2.append(np.ravel_multi_index(bins2[idx], bins2d_shape))
+    linear_indices2 = bin_spikes(
+        samples2,
+        channels2,
+        samples_binsize=samples_binsize,
+        channels_binsize=channels_binsize,
+        fs=fs,
+        num_channels=num_channels,
+        max_samples=max_samples,
+    )
 
     # find the intersection of the two spike trains' bin indices for each shift
     bin_linear_indices = []
@@ -105,3 +87,144 @@ def spiketrain_intersect(
     indices2_foundby1 = reduce(np.union1d, indices2)
 
     return common_spike_bins, indices1_foundby2, indices2_foundby1
+
+
+def bin_spikes(
+    samples,
+    channels,
+    samples_binsize=None,
+    channels_binsize=4,
+    fs=30000,
+    num_channels=384,
+    max_samples=None,
+):
+    if not samples_binsize:
+        # Default: 0.4 ms
+        samples_binsize = int(0.4 * fs / 1000)
+
+    if not max_samples:
+        max_samples = np.max(samples)
+
+    assert samples.shape[0] == channels.shape[0]
+
+    # array of bin shifts in time and channel directions
+    # applied to catch spikes on either side of bin borders
+    shifts = np.array(
+        [
+            [0, 0],
+            [samples_binsize // 2, channels_binsize // 2],
+            [samples_binsize // 2, 0],
+            [0, channels_binsize // 2],
+        ]
+    )
+
+    # we'll divide by this factor and take the floor to find the sample and channel bin
+    # for each spike
+    factor = np.array([[samples_binsize, channels_binsize] * 4], dtype=np.float64)
+
+    bins2d_shape = (
+        int(max_samples // samples_binsize) + 2,
+        int(num_channels // channels_binsize) + 2,
+    )
+
+    # tile data over the 4 possible shift conditions
+    # What we end up with is an array that looks like:
+    # [sample_bins_shift(0,0)]
+    # [channel_bins_shift(0,0)]
+    # [sample_bins_shift(1,0)]
+    # ...
+    data = np.tile(np.vstack((samples, channels)), (4, 1))
+    bins = np.floor((data + shifts.flatten()[:, None]) / factor.T).astype(int)
+
+    # store the ravelled (sample bin, channel bin) indices for each shift
+    linear_indices = []
+    for i in range(4):
+        idx = slice(i * 2, (i + 1) * 2)
+        linear_indices.append(np.ravel_multi_index(bins[idx], bins2d_shape))
+
+    return np.array(linear_indices)
+
+
+def spikes_per_bin(
+    samples_tuple,
+    channels_tuple,
+    samples_binsize=None,
+    channels_binsize=4,
+    fs=30000,
+    num_channels=384,
+):
+    """
+    :param samples: A tuple of N sample times of spikes (each a 1D NumPy array)
+    :param channels: A tuple of N channel locations of spikes (each a 1D NumPy array)
+    """
+
+    assert len(samples_tuple) == len(
+        channels_tuple
+    ), "samples and channels must have same length."
+    assert all(
+        samples_tuple[i].shape == channels_tuple[i].shape
+        for i in range(len(samples_tuple))
+    ), "For each spike sorter, each spike must have exactly one sample time and one channel."
+
+    max_samples = max([np.max(samples) for samples in samples_tuple])
+
+    if not samples_binsize:
+        # Default: 0.4 ms
+        samples_binsize = int(0.4 * fs / 1000)
+
+    bins2d_shape = (
+        int(max_samples // samples_binsize) + 2,
+        int(num_channels // channels_binsize) + 2,
+    )
+
+    num_bins = bins2d_shape[0] * bins2d_shape[1]
+
+    # store binning for each sorter
+    linear_indices = [None] * len(samples_tuple)
+
+    for i, samples_array in enumerate(samples_tuple):
+        channels_array = channels_tuple[i]
+        linear_indices[i] = bin_spikes(
+            samples_array,
+            channels_array,
+            samples_binsize=samples_binsize,
+            channels_binsize=channels_binsize,
+            fs=fs,
+            num_channels=num_channels,
+            max_samples=max_samples,
+        )
+
+    # for each set of four binnings (four shifts), poll for each spike to determine
+    # the bin to assign it to.
+    polled_binnings = [None] * len(samples_tuple)
+
+    for i, binning in enumerate(linear_indices):
+        # length is number of spikes
+        _arr = np.zeros(binning.shape[1])
+        # most common bin each spike is assigned to
+        polled_binnings[i] = mode(binning, axis=0, keepdims=False)[0]
+
+    
+    # [i, j] is the number of spikes found by sorter i in bin j
+    bin_counts = np.zeros((len(samples_tuple), num_bins), dtype=int)
+    for i, polled in enumerate(polled_binnings):
+        bin_idx, counts = np.unique(polled, return_counts=True)
+        bin_counts[i, bin_idx] = counts
+        continue
+
+    return bin_counts
+
+def spikes_venn(samples_tuple,
+    channels_tuple,
+    samples_binsize=None,
+    channels_binsize=4,
+    fs=30000,
+    num_channels=384,):
+
+    bin_counts = spikes_per_bin(samples_tuple, channels_tuple, 
+                                samples_binsize=samples_binsize,
+                                channels_binsize=channels_binsize,
+                                fs=fs,
+                                num_channels=num_channels
+                                )
+    
