5777113eeac2df0ced686dd9102cadb5f41fea36
Gaelle <gaelle.chapuis.ibl@gmail.com>
2024-02-27 14:42:59 +0000

    flake8

--- src/ibldsp/waveforms.py
+++ src/ibldsp/waveforms.py
@@ -563,7 +563,7 @@ def extract_wfs_array(arr, df, channel_neighbors, trough_offset=42, spike_length
     :param spike_length_samples: Total length of wf in samples.
     (defaults to 121)
     """
-    ## Add a last row filled with nan
+    # Add a last row filled with nan
     newrow = np.empty((1, arr.shape[1]))
     newrow[:] = np.nan
     arr = np.vstack([arr, newrow])
