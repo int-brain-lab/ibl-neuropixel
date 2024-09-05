5901ff8675c62ecab588b2f7541d696b42ca31c9
chris-langfield <christopher.langfield@internationalbrainlab.org>
2024-09-05 10:02:37 -0400

    throw correct warning

--- src/neurodsp/__init__.py
+++ src/neurodsp/__init__.py
@@ -3,6 +3,6 @@ from warnings import warn
 
 sys.modules["neurodsp"] = ibldsp
 warn(
-    "neurodsp has been renamed to ibldsp and the old name will be deprecated on 01-Sep-2024.",
+    "neurodsp has been renamed to ibldsp and the old name will be deprecated on 01-Oct-2024.",
     FutureWarning,
 )
