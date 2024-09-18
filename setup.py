301ef2d21051254195d37cc6b3107f01981cb890
chris-langfield <christopher.langfield@internationalbrainlab.org>
2024-09-18 10:08:06 -0400

    bumpversion + release notes

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setuptools.setup(
     name="ibl-neuropixel",
-    version="1.3.1",
+    version="1.3.2",
     author="The International Brain Laboratory",
     description="Collection of tools for Neuropixel 1.0 and 2.0 probes data",
     long_description=long_description,
